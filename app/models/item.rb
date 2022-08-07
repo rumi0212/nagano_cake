e9bbf7f8ec21afb48ced7ba51de8a00a4a162b64
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-08-07 14:42:05 +0000

    [Add]データベース作成

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  has_one_attached : image
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+  has_many :orders, through: :order_details
+  belongs_to :genre
+  
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :genre_id, presence: true
+  
+end
