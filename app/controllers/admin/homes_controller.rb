491c13d98baefb51321dd6f269acfeb8c04a219a
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-08-19 13:17:41 +0000

    [Add]管理者トップページ（注文履歴一覧画面）

---
+++ app/controllers/admin/homes_controller.rb
@@ -0,0 +1,5 @@
+class Admin::HomesController < ApplicationController
+  def top
+    @orders = Order.all.page(params[:page]).per(10).order(created_at: :desc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :admin do
+    root to: "homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, except: [:destroy]
     resources :customers, only: [:index, :show, :edit, :update]
