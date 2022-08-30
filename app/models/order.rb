17ea01140751845b7bfdf64235e325416fa86a83
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-08-30 13:16:10 +0000

    [Add]カート機能実装/enum導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :items, through: :order_detail
 
-  enum payment_method: { クレジットカード: 0,銀行振込: 1}
+  enum payment_method: { credit_card: 0, transfer: 1 }
 
 
   enum order_status: {
