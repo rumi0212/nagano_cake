647a0e634559e69454e21a6fa2ab65c005f5485e
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-09-07 13:02:54 +0000

    [Add]レイアウト調整

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,21 +1,14 @@
 class Public::CartItemsController < ApplicationController
-  
+
   before_action :authenticate_customer!
 
   def index
     @cart_items= current_customer.cart_items.all
+    @subtotal = 0
   end
-  
-  def with_tax_price
-    (price * 1.1).floor
-  end
-  
-  def subtotal
-    item.with_tax_price * amount
-  end
-  
+
   def create
-    @cart_item = CartItem.new(cart_items_params)
+    @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_items=current_customer.cart_items.all
       @cart_items.each do |cart_item|
@@ -25,34 +18,34 @@ class Public::CartItemsController < ApplicationController
         @cart_item.delete
         end
       end
-      
+
       @cart_item.save
       redirect_to cart_items_path,notice:"カートに商品が入りました"
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(count: params[:cart_item][:count].to_i)
     @cart_item.save
     redirect_to cart_items_path
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_items_path
   end
-  
+
   def all_destroy
     @cart_items = current_customer.cart_items
     @cart_items.destroy_all
     redirect_to cart_items_path
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:amount, :item_id)
   end
-  
+
 end
